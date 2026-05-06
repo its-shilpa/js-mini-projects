86931ba5f768ededeb68bf2bbc0e9d60d5e1143a
Shilpa Mukherjee <shilpamukherjee625@gmail.com>
2026-05-07 00:29:36 +0530

    Fixed the height issue of the number guessing game

--- js/color-changer.js
+++ js/color-changer.js
@@ -3,6 +3,29 @@ const body = document.querySelector('body');
 
 buttons.forEach(function (btn) {
   btn.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+
+    //using if else condition:
+
+    // if (e.target.id === 'grey') {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id === 'white') {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id === 'blue') {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id === 'yellow') {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id === 'purple') {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+
+    //using switch case:
+
     const color = e.target.id;
 
     switch (color) {
